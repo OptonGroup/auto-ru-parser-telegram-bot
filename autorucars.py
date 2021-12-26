b4b1664a9790678c028fa1f894803bfab3467137
MXCode <maxsimakov123@yandex.ru>
2021-12-27 00:24:49 +0400

    main

---
+++ autorucars.py
@@ -0,0 +1,45 @@
+from requests import Session
+from bs4 import BeautifulSoup
+from json import loads
+import time
+
+class autorucars(object):
+    def __init__(self, url):
+        self.url = url
+        self.watched_cars = set()
+        self.cars = {}
+        self.page = 0
+        self.headers = {
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.113 Safari/537.36'
+        }
+        self.max_page = -1
+
+    def __next_page(self):
+        session = Session()
+        response = session.get(url=self.url+str(self.page), headers=self.headers)
+        response.encoding = 'utf-8'
+        cars_json = loads(BeautifulSoup(response.text, 'lxml').find('script').text)['offers']['offers']
+        cars = set(tuple(el.values()) for el in cars_json)
+
+        if (self.max_page == -1):
+            max_page = BeautifulSoup(response.text, 'lxml').find_all('a', class_='ListingPagination__page')
+            if (max_page):
+                self.max_page = max_page[-1].text
+            else:
+                self.max_page = 1
+        
+        cars -= self.watched_cars
+        self.watched_cars |= cars
+        return cars
+
+    def next_car(self):
+        if (len(self.cars) == 0):
+            self.page += 1
+            if (self.max_page != -1 and self.page > self.max_page):
+                return False
+            self.cars = self.__next_page()
+            if (len(self.cars) == 0):
+                return False
+        
+        return self.cars.pop()
--- main.py
+++ main.py
@@ -1,82 +1,52 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
 from config import TOKEN
-import requests
-from bs4 import BeautifulSoup
-
-headers = {
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.113 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language':'ru-ru,ru;q=0.8,en-us;q=0.5,en;q=0.3',
-    'Accept-Encoding':'gzip, deflate',
-    'Connection':'keep-alive',
-    'DNT':'1'
-}
+from config import URL
+from autorucars import autorucars
 
 button_next_auto = InlineKeyboardMarkup().add(InlineKeyboardButton("Следующий автомобиль", callback_data="next_auto"))
 
-
-def parse_site(page):
-    site_content = session.get('https://auto.ru/cars/nissan/all/?page={page}&output_type=carousel', headers=headers)
-    site_content.encoding = 'utf-8'
-    site_content = BeautifulSoup(site_content.text, 'lxml')
-    site_content.find('a').click()
-    site_content = session.get('https://auto.ru/cars/nissan/all/?page={page}&output_type=carousel', headers=headers)
-    site_content.encoding = 'utf-8'
-    site_content = BeautifulSoup(site_content.text, 'lxml')
-    print(site_content)
-    
-    output = []
-    cars = [el for el in site_content.find_all('div', class_="ListingItemWide")]
-    if len(cars) > 0:
-        output.append('Нашёл для тебя прекрасную подборку автомобилей😄')
-    else:
-        return ['К сожелению подборка пуста😔']
-        
-    for car in cars:
-        name = car.h3.text
-        cost = car.find('div', class_='ListingItemPrice__content').span.text
-        link = car.a['href']
-        output.append(f'\n Название: {name} \nЦена: {cost} \nСсылка: {link}')
-    
-    return output
-
-
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
-session = requests.Session()
-session.get('https://auto.ru', headers=headers)
 
-cars = []
-cnt = 1
+cars = set()
 
 @dp.message_handler(commands=['refresh'])
 async def parse(message: types.Message):
-    global cars, cnt
-    cnt = 1
-    cars = parse_site(cnt)
-    await bot.send_message(message.from_user.id, cars[0])
-    cars = cars[1:]
-    if (len(cars) == 0):
-        return
-    car = cars.pop()
-    await bot.send_message(message.from_user.id, car, reply_markup=button_next_auto)
+    global cars
+    cars = autorucars(URL)
+    
+    car = cars.next_car()
+    while 1:
+        if (car == False):
+            break
+        if (len(car) == 7):
+            break
+        car = cars.next_car()
+    if car != False:
+        await bot.send_message(message.from_user.id, 'Нашёл для тебя прекрасную подборку автомобилей😄')
+        await bot.send_message(message.from_user.id, f'Название: {car[2]}\nЦена: {car[3]}₽\nСсылка: {car[1]}', reply_markup=button_next_auto)
+    else:
+        await bot.send_message(message.from_user.id, 'К сожелению подборка пуста😔')
     
 @dp.callback_query_handler()
 async def next_auto_function(callback_query: types.CallbackQuery):
-    global cars, cnt
     if (callback_query.data == 'next_auto'):
-        if (len(cars) == 0):
-            cnt+=1
-            cars = parse_site(cnt)[1:]
-            if (len(cars) == 0):
-                await bot.send_message(callback_query.from_user.id, 'Автомобили в подборке закончились')
-                return
-        
-        car = cars.pop()
-        await callback_query.message.edit_text(car, reply_markup=button_next_auto)
+        if (cars == set()):
+            await callback_query.message.edit_text('Пропишите /refresh ещё раз')
+            return
+        car = cars.next_car()
+        while 1:
+            if (car == False):
+                break
+            if (len(car) == 7):
+                break
+            car = cars.next_car()
+        if car != False:
+            await callback_query.message.edit_text(f'Название: {car[2]}\nЦена: {car[3]}₽\nСсылка: {car[1]}', reply_markup=button_next_auto)
+        else:
+            await callback_query.message.edit_text('Автомобили в подборке закончились')
 
 
 if __name__== "__main__":
